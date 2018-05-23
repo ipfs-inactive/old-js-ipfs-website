eed6f7cdcfc98e62072cade63bea1890d86cae7b
Pedro Santos <pedro.santos@moxy.studio>
2018-05-23 16:40:45 +0100

    feat: add banner

--- src/shared/components/banner/index.js
+++ src/shared/components/banner/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import closeSvg from 'shared/media/icons/close-button.svg';
+import alphaSvg from 'shared/media/icons/alpha-sign.svg';
 import styles from './index.module.css';
 
 const Banner = () => (
     <div className={ styles.container }>
-        <div className={ styles.icon }>α</div>
-        <div className={ styles.text }>
-            Our app is in Alpha state. The codebase hasnt been audited by
-            Security specialists and it shouldnt be used to store, share or publish sensitive information
+        <div className={ styles.iconContainer }>
+            <img src={ alphaSvg } />
         </div>
-        <div className={ styles.closeButton }>
-            x
+        <div className={ styles.text }>
+            <p>
+                <b><FormattedMessage id="bannerHighlightMessage" />.</b> <FormattedMessage id="bannerMessage" />
+            </p>
+        </div>
+        <div className={ styles.closeButtonContainer }>
+            <img src={ closeSvg } />
         </div>
     </div>
 );
