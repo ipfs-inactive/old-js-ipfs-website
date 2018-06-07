0f103b37bda713736ff6e8fe94e4b7ff8668e37e
Pedro Santos <pedro.santos@moxy.studio>
2018-06-07 15:39:52 +0100

    feat: add footer

--- src/shared/components/footer/index.js
+++ src/shared/components/footer/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import classNames from 'classnames';
+
+import ProtocolIcon from '-!svg-react-loader!shared/media/images/pl-logo.svg';
 import styles from './index.module.css';
 
 const Footer = ({ className }) => (
     <footer className={ classNames(styles.footer, className) }>
-        <div
-            style={ {
-                margin: '0 auto',
-                maxWidth: 960,
-                padding: '1.45rem 1.0875rem',
-            } }>
-            This is the footer
+        <div className={ styles.container }>
+            <div className={ styles.leftContent }>© Protocol Labs | Except as noted, content licensed CC-BY 3.0</div>
+            <div className={ styles.rightContent }>
+                <div className={ styles.text }>IPFS.JS was started and is sponsored by</div>
+                <ProtocolIcon />
+            </div>
         </div>
     </footer>
 );
