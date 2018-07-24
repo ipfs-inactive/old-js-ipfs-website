f94fb7a98edf19ca18d5e6b73e6b305b624e8d25
Pedro Santos <pedro.santos@moxy.studio>
2018-07-24 10:53:39 +0100

    feat: add service worker svg and its animation

---
+++ src/shared/components/service-worker-section/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { injectIntl } from 'react-intl'
+import classNames from 'classnames'
+
+import ServiceWorkerSvg from 'shared/media/images/service-worker1.svg'
+import ToggleButton from 'shared/components/toggle-button'
+import styles from './index.module.css'
+
+class ServiceWorkerSection extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      isActive: true
+    }
+  }
+
+  render () {
+    const { isActive } = this.state
+    const contentClasses = classNames(styles.content, {
+      [styles.active]: isActive
+    })
+
+    return (
+      <div className={ styles.container }>
+        <div className={ contentClasses }>
+          <ServiceWorkerSvg />
+          <ToggleButton isActive={ isActive } onClick={ this.handleToggleClick }/>
+        </div>
+      </div>
+    )
+  }
+
+  handleToggleClick = () => {
+    const { isActive } = this.state
+    this.setState({
+      isActive: !isActive
+    })
+  }
+}
+
+export default injectIntl(ServiceWorkerSection)
