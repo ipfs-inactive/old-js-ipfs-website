26510b77c9ed30043cdb5cef8ce84b43e8e46c7a
Pedro Santos <pedro.santos@moxy.studio>
2018-07-26 12:43:15 +0100

    chore: add projects to what people are building section

--- src/shared/data/what-are-people-building/index.js
+++ src/shared/data/what-are-people-building/index.js
@@ -1,20 +1,49 @@
 import React from 'react'
 
-import ParatiiSvg from 'shared/media/images/paratii.svg'
-import ParatiiPrintScreen from 'shared/media/images/paratii-ps.png'
+// import CompanionSvg from 'shared/media/images/what-are-people-building/companion.svg'
+import PeerpadSvg from 'shared/media/images/what-are-people-building/peerpad.svg'
+import ParatiiSvg from 'shared/media/images/what-are-people-building/paratii.svg'
+
+import CompanionPrintScreen from 'shared/media/images/what-are-people-building/companion.jpg'
+import FilenationPrintScreen from 'shared/media/images/what-are-people-building/filenation.jpg'
+import PeerpadPrintScreen from 'shared/media/images/what-are-people-building/peerpad.jpg'
+import DwebArchivePrintScreen from 'shared/media/images/what-are-people-building/dwebarchive.jpg'
+import OrbitPrintScreen from 'shared/media/images/what-are-people-building/orbitchat.jpg'
+import ParatiiPrintScreen from 'shared/media/images/what-are-people-building/paratii.jpg'
 
 const projects = [
   {
     translationListIndex: 0,
-    icon: <ParatiiSvg />,
-    image: ParatiiPrintScreen,
-    link: 'https://paratii.video/'
+    // icon: <CompanionSvg />,
+    image: CompanionPrintScreen,
+    link: 'https://github.com/ipfs-shipyard/ipfs-companion'
   },
   {
     translationListIndex: 1,
+    image: FilenationPrintScreen,
+    link: 'https://github.com/FileNation/FileNation'
+  },
+  {
+    translationListIndex: 2,
+    icon: <PeerpadSvg />,
+    image: PeerpadPrintScreen,
+    link: 'https://github.com/ipfs-shipyard/peer-pad'
+  },
+  {
+    translationListIndex: 3,
+    image: DwebArchivePrintScreen,
+    link: 'https://github.com/internetarchive/dweb-archive'
+  },
+  {
+    translationListIndex: 4,
+    image: OrbitPrintScreen,
+    link: 'https://github.com/orbitdb/orbit'
+  },
+  {
+    translationListIndex: 5,
     icon: <ParatiiSvg />,
     image: ParatiiPrintScreen,
-    link: 'https://paratii.video/'
+    link: 'https://github.com/Paratii-Video/paratii-portal'
   }
 ]
 
