ff7ddddede90dfed1dac181e9b02c77e2453fc02
David Dias <daviddias.p@gmail.com>
2018-07-30 10:50:41 -0700

    feat: french version by camillecoyac

--- intl/config.js
+++ intl/config.js
@@ -3,6 +3,7 @@ module.exports = {
   availableLocales: [
     { acronym: 'en', fullForm: 'English' },
     { acronym: 'pt', fullForm: 'Português (PT)' },
-    { acronym: 'it', fullForm: 'Italiano' }
+    { acronym: 'it', fullForm: 'Italiano' },
+    { acronym: 'fr', fullForm: 'Français' }
   ]
 }
