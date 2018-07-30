f325a76ae1302934b9cb295ff4ccf5c7f0d32bef
André Cruz <andremiguelcruz@msn.com>
2018-07-30 01:09:39 +0100

    chore: ensure consistency of curly braces in children

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -16,7 +16,7 @@ const Home = () => (
     <Features />
     <GettingStarted />
     <Gateway />
-    {/* <WhatCanYouBuild /> */}
+    { /* <WhatCanYouBuild /> */ }
     <WhatArePeopleBuilding />
     <PublicationsAndTalks />
     <Community />
