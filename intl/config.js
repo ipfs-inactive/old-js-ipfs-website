3b812bda8d4b0781d4051482e7539e63dd00d69c
David Dias <daviddias.p@gmail.com>
2018-07-30 20:48:53 +0200

    Italian (#105)
    * Adding Italian translation (#103)
    
    * adding italian translation
    
    * Update it.json
    
    * feat: update config to add italian support

--- intl/config.js
+++ intl/config.js
@@ -2,6 +2,7 @@ module.exports = {
   defaultLocale: 'en',
   availableLocales: [
     { acronym: 'en', fullForm: 'English' },
-    { acronym: 'pt', fullForm: 'Português (PT)' }
+    { acronym: 'pt', fullForm: 'Português (PT)' },
+    { acronym: 'it', fullForm: 'Italiano' }
   ]
 }
