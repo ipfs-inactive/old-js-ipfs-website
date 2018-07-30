5e6bc4e02f82d43cbb58c275fdac8fe81043904c
Pedro Santos <pedro.santos@moxy.studio>
2018-07-30 15:44:09 +0100

    feat: add gateway svg for mobile resolutions

--- src/shared/components/gateway-section/gateway-svg-animation/index.js
+++ src/shared/components/gateway-section/gateway-svg-animation/index.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
+import MobileGatewaySvg from 'shared/media/images/gateway-section/mobile-gateway.svg'
+import MobileGatewayBeams1 from 'shared/media/images/gateway-section/mobile-gateway-beams-1.svg'
+import MobileGatewayBlueBeams from 'shared/media/images/gateway-section/mobile-gateway-blue-beams.svg'
+
+import MobileServiceWorkerBeam1 from 'shared/media/images/gateway-section/mobile-serviceworker-yellowbeam-1.svg'
+import MobileServiceWorkerBeam2 from 'shared/media/images/gateway-section/mobile-serviceworker-yellowbeam-2.svg'
+import MobileServiceWorkerArrowBeams from 'shared/media/images/gateway-section/mobile-serviceworker-arrow-beams.svg'
+
+import MobileBeamsNodes1 from 'shared/media/images/gateway-section/mobile-beams-nodes-1.svg'
+import MobileBeamsNodes2 from 'shared/media/images/gateway-section/mobile-beams-nodes-2.svg'
+import MobileBeamsNodes3 from 'shared/media/images/gateway-section/mobile-beams-nodes-3.svg'
+import MobileBeamsNodes4 from 'shared/media/images/gateway-section/mobile-beams-nodes-4.svg'
+
 import GatewaySvg from 'shared/media/images/gateway-section/gateway.svg'
 import GatewayBlueBeams from 'shared/media/images/gateway-section/gateway-blue-beams.svg'
 import GatewayWhiteBeam1 from 'shared/media/images/gateway-section/gateway-whitebeam-1.svg'
@@ -31,7 +44,6 @@ import styles from './index.module.css'
 class GatewaySvgAnimation extends Component {
   render () {
     const { isActive, inView } = this.props
-
     const containerClasses = classNames(styles.container, {
       [styles.active]: isActive,
       [styles.animationOff]: !inView
@@ -39,75 +51,116 @@ class GatewaySvgAnimation extends Component {
 
     return (
       <div className={ containerClasses }>
-        <GatewaySvg />
-        <div className={ styles.gatewayBlueBeams }>
-          <GatewayBlueBeams />
-        </div>
-        <div className={ styles.gatewayWhiteBeam1 }>
-          <GatewayWhiteBeam1 />
-        </div>
-        <div className={ styles.gatewayWhiteBeam2 }>
-          <GatewayWhiteBeam2 />
-        </div>
-        <div className={ styles.serviceWorkerBeam1 }>
-          <ServiceWorkerBeam1 />
-        </div>
-        <div className={ styles.serviceWorkerBeam2 }>
-          <ServiceWorkerBeam2 />
-        </div>
-        <div className={ styles.serviceWorkerBeam3 }>
-          <ServiceWorkerBeam3 />
-        </div>
-        <div className={ styles.serviceWorkerBeam4 }>
-          <ServiceWorkerBeam4 />
-        </div>
-        <div className={ styles.serviceWorkerArrowBeams }>
-          <ServiceWorkerArrowBeams />
-        </div>
-        <div className={ styles.beamsNodes1 }>
-          <BeamsNodes1 />
-        </div>
-        <div className={ styles.beamsNodes2 }>
-          <BeamsNodes2 />
-        </div>
-        <div className={ styles.beamsNodes3 }>
-          <BeamsNodes3 />
-        </div>
-        <div className={ styles.beamsNodes4 }>
-          <BeamsNodes4 />
-        </div>
-        <div className={ styles.beamsNodes5 }>
-          <BeamsNodes5 />
-        </div>
-        <div className={ styles.beamsNodes6 }>
-          <BeamsNodes6 />
-        </div>
-        <div className={ styles.beamsNodes7 }>
-          <BeamsNodes7 />
-        </div>
-        <div className={ styles.beamsNodes8 }>
-          <BeamsNodes8 />
-        </div>
-        <div className={ styles.beamsNodes9 }>
-          <BeamsNodes9 />
-        </div>
-        <div className={ styles.beamsNodes10 }>
-          <BeamsNodes10 />
-        </div>
-        <div className={ styles.beamsNodes11 }>
-          <BeamsNodes11 />
-        </div>
-        <div className={ styles.beamsNodes12 }>
-          <BeamsNodes12 />
-        </div>
+        { this.renderDesktopGateway() }
+        { this.renderMobileGateway() }
       </div>
     )
   }
+
+  renderDesktopGateway = () => (
+    <div className={ styles.desktop }>
+      <GatewaySvg />
+      <div className={ styles.gatewayBlueBeams }>
+        <GatewayBlueBeams />
+      </div>
+      <div className={ styles.gatewayWhiteBeam1 }>
+        <GatewayWhiteBeam1 />
+      </div>
+      <div className={ styles.gatewayWhiteBeam2 }>
+        <GatewayWhiteBeam2 />
+      </div>
+      <div className={ styles.serviceWorkerBeam1 }>
+        <ServiceWorkerBeam1 />
+      </div>
+      <div className={ styles.serviceWorkerBeam2 }>
+        <ServiceWorkerBeam2 />
+      </div>
+      <div className={ styles.serviceWorkerBeam3 }>
+        <ServiceWorkerBeam3 />
+      </div>
+      <div className={ styles.serviceWorkerBeam4 }>
+        <ServiceWorkerBeam4 />
+      </div>
+      <div className={ styles.serviceWorkerArrowBeams }>
+        <ServiceWorkerArrowBeams />
+      </div>
+      <div className={ styles.beamsNodes1 }>
+        <BeamsNodes1 />
+      </div>
+      <div className={ styles.beamsNodes2 }>
+        <BeamsNodes2 />
+      </div>
+      <div className={ styles.beamsNodes3 }>
+        <BeamsNodes3 />
+      </div>
+      <div className={ styles.beamsNodes4 }>
+        <BeamsNodes4 />
+      </div>
+      <div className={ styles.beamsNodes5 }>
+        <BeamsNodes5 />
+      </div>
+      <div className={ styles.beamsNodes6 }>
+        <BeamsNodes6 />
+      </div>
+      <div className={ styles.beamsNodes7 }>
+        <BeamsNodes7 />
+      </div>
+      <div className={ styles.beamsNodes8 }>
+        <BeamsNodes8 />
+      </div>
+      <div className={ styles.beamsNodes9 }>
+        <BeamsNodes9 />
+      </div>
+      <div className={ styles.beamsNodes10 }>
+        <BeamsNodes10 />
+      </div>
+      <div className={ styles.beamsNodes11 }>
+        <BeamsNodes11 />
+      </div>
+      <div className={ styles.beamsNodes12 }>
+        <BeamsNodes12 />
+      </div>
+    </div>
+  )
+
+  renderMobileGateway = () => (
+    <div className={ styles.mobile }>
+      <MobileGatewaySvg />
+      <div className={ styles.gatewayBlueBeams }>
+        <MobileGatewayBlueBeams />
+      </div>
+      <div className={ styles.gatewayWhiteBeam1 }>
+        <MobileGatewayBeams1 />
+      </div>
+      <div className={ styles.serviceWorkerBeam1 }>
+        <MobileServiceWorkerBeam1 />
+      </div>
+      <div className={ styles.serviceWorkerBeam2 }>
+        <MobileServiceWorkerBeam2 />
+      </div>
+      <div className={ styles.serviceWorkerArrowBeams }>
+        <MobileServiceWorkerArrowBeams />
+      </div>
+      <div className={ styles.beamsNodes1 }>
+        <MobileBeamsNodes1 />
+      </div>
+      <div className={ styles.beamsNodes2 }>
+        <MobileBeamsNodes2 />
+      </div>
+      <div className={ styles.beamsNodes3 }>
+        <MobileBeamsNodes3 />
+      </div>
+      <div className={ styles.beamsNodes4 }>
+        <MobileBeamsNodes4 />
+      </div>
+    </div>
+  )
 }
 
 GatewaySvgAnimation.propTypes = {
   isActive: PropTypes.bool.isRequired,
-  inView: PropTypes.bool
+  inView: PropTypes.bool,
+  isMobile: PropTypes.bool
 }
 
 export default GatewaySvgAnimation
