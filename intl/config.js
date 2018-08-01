097543c99f476c003939096fc307a03f0860f526
David Dias <daviddias.p@gmail.com>
2018-07-31 22:44:09 -0700

    fix: remove incomplete PT

--- intl/config.js
+++ intl/config.js
@@ -2,7 +2,6 @@ module.exports = {
   defaultLocale: 'en',
   availableLocales: [
     { acronym: 'en', fullForm: 'English' },
-    { acronym: 'pt', fullForm: 'Português (PT)' },
     { acronym: 'pl', fullForm: 'Polski' },
     { acronym: 'it', fullForm: 'Italiano' },
     { acronym: 'zh', fullForm: '简体中文' },
