8573d8e6a2b5fa5984c33423c33b218b35dcfb35
Marcin Rataj <lidel@lidel.org>
2018-07-31 18:28:55 -0700

    feat: add Polish translation (#115)
    Quick initial version, needs to be eyeballed by someone else :-)

--- intl/config.js
+++ intl/config.js
@@ -3,6 +3,7 @@ module.exports = {
   availableLocales: [
     { acronym: 'en', fullForm: 'English' },
     { acronym: 'pt', fullForm: 'Português (PT)' },
+    { acronym: 'pl', fullForm: 'Polski' },
     { acronym: 'it', fullForm: 'Italiano' },
     { acronym: 'zh', fullForm: '简体中文' }
   ]
