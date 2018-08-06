2ae528fed40412c627663004785213a0e92904d2
Pedro Santos <pedro.santos@moxy.studio>
2018-08-06 14:03:39 +0100

    feat: add favicon and manifest files

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,28 @@ module.exports = {
         include: /media/
       }
     },
-    'gatsby-plugin-ipfs'
+    'gatsby-plugin-ipfs',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'JS.IPFS',
+        short_name: 'JS.IPFS',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }
   ]
 }
