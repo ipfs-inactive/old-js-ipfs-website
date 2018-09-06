01d7322c4d543ee1244a86834ec132001fcf67c5
Pedro Santos <pedro.santos@moxy.studio>
2018-09-06 16:38:39 +0100

    feat: add split code view on desktop and code view on mobile

--- src/shared/components/what-you-can-build-section/index.js
+++ src/shared/components/what-you-can-build-section/index.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react'
 import ReactMarkdown from 'react-markdown'
 import { injectIntl } from 'react-intl'
 
+import withMobileSizeDetection from 'shared/components/with-mobile-size-detection'
 import NavPill from 'shared/components/nav-pill'
 import appsArr from 'shared/data/what-you-can-build'
 import HorizontalScroller from 'shared/components/horizontal-scroller'
@@ -13,9 +14,10 @@ class WhatYouCanBuild extends PureComponent {
   }
 
   render () {
-    const { messages } = this.props.intl
+    const { isMobile, intl: { messages } } = this.props
     const { activePillIndex } = this.state
     const activeLink = appsArr[activePillIndex].link
+    const editorView = isMobile ? 'editor' : 'split'
     const navPills = appsArr.map((app, index) => {
       return (
         <NavPill
@@ -35,7 +37,7 @@ class WhatYouCanBuild extends PureComponent {
           <ReactMarkdown className={ styles.sectionDescription } source={ messages.whatYouCanBuild.sectionDesc } />
           <HorizontalScroller itemsArray={ navPills }/>
           <div className={ styles.iframeContainer }>
-            <iframe src={ `${activeLink}?view=split` }
+            <iframe src={ `${activeLink}?view=${editorView}` }
               sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin" />
           </div>
         </div>
@@ -50,4 +52,4 @@ class WhatYouCanBuild extends PureComponent {
   }
 }
 
-export default injectIntl(WhatYouCanBuild)
+export default withMobileSizeDetection(injectIntl(WhatYouCanBuild))
