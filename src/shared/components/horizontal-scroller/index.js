e87cd6573d7d7f5c805680e49edda701774f4804
Pedro Santos <pedro.santos@moxy.studio>
2018-09-10 11:40:50 +0100

    feat: add loading container while codesandbox is loading

--- src/shared/components/horizontal-scroller/index.js
+++ src/shared/components/horizontal-scroller/index.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 import styles from './index.module.css'
 
-const HorizontalScroller = ({ itemsArray }) => {
+const HorizontalScroller = ({ itemsArray, className }) => {
+  const scrollerClasses = classNames(styles.container, className)
   return (
-    <div className={ styles.container }>
+    <div className={ scrollerClasses }>
       { itemsArray }
     </div>
   )
 }
 
 HorizontalScroller.propTypes = {
-  itemsArray: PropTypes.array.isRequired
+  itemsArray: PropTypes.array.isRequired,
+  className: PropTypes.string
 }
 
 export default HorizontalScroller
--- src/shared/components/what-you-can-build-section/index.js
+++ src/shared/components/what-you-can-build-section/index.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import ReactMarkdown from 'react-markdown'
 import { injectIntl } from 'react-intl'
+import classNames from 'classnames'
 
 import withMobileSizeDetection from 'shared/components/with-mobile-size-detection'
 import NavPill from 'shared/components/nav-pill'
@@ -10,45 +11,70 @@ import styles from './index.module.css'
 
 class WhatYouCanBuild extends PureComponent {
   state = {
-    activePillIndex: 0
+    activePillIndex: 0,
+    isIframeLoaded: false
   }
 
   render () {
     const { isMobile, intl: { messages } } = this.props
-    const { activePillIndex } = this.state
+    const { activePillIndex, isIframeLoaded } = this.state
     const activeLink = appsArr[activePillIndex].link
     const editorView = isMobile ? 'editor' : 'split'
-    const navPills = appsArr.map((app, index) => {
-      return (
-        <NavPill
-          key={ `app-${index}` }
-          index={ index }
-          title={ app.title }
-          active={ activePillIndex === index }
-          onPillClick={ this.handlePillClick }
-        />
-      )
-    })
 
     return (
       <div className={ styles.container }>
         <div className={ styles.content }>
           <h1>{ messages.whatYouCanBuild.sectionTitle }</h1>
           <ReactMarkdown className={ styles.sectionDescription } source={ messages.whatYouCanBuild.sectionDesc } />
-          <HorizontalScroller itemsArray={ navPills }/>
-          <div className={ styles.iframeContainer }>
-            <iframe src={ `${activeLink}?view=${editorView}` }
-              sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin" />
-          </div>
+          <HorizontalScroller className={ styles.horizontalScroller } itemsArray={ this.getNavPills() }/>
+          { this.renderIframeContainer(isIframeLoaded, activeLink, editorView) }
         </div>
       </div>
     )
   }
 
-  handlePillClick = (index) => {
-    this.setState({
-      activePillIndex: index
+  renderIframeContainer = (isIframeLoaded, activeLink, editorView) => {
+    const loadingContainerClasses = classNames(styles.loadingContainer, {
+      [styles.hideLoading]: isIframeLoaded
     })
+
+    return (
+      <div className={ styles.iframeContainer }>
+        <iframe src={ `${activeLink}?view=${editorView}` }
+          sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+          onLoad={ this.handleIframeLoad } />
+        <div className={ loadingContainerClasses }>
+          <div className={ styles.loadingCircle } />
+          <span>Loading, please wait...</span>
+        </div>
+      </div>
+    )
+  }
+
+  getNavPills = () =>
+    appsArr.map((app, index) => {
+      return (
+        <NavPill
+          key={ `app-${index}` }
+          index={ index }
+          title={ app.title }
+          active={ this.state.activePillIndex === index }
+          onPillClick={ this.handlePillClick }
+        />
+      )
+    })
+
+  handleIframeLoad = () => {
+    this.setState({
+      isIframeLoaded: true
+    })
+  }
+
+  handlePillClick = (index) => {
+    this.setState(({ activePillIndex }) => ({
+      activePillIndex: index,
+      isIframeLoaded: activePillIndex === index
+    }))
   }
 }
 
