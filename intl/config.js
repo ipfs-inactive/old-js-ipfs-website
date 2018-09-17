21f738cc2c834c4ccf5825532dc9a693f99f9f9c
Marcin Rataj <lidel@lidel.org>
2018-09-17 21:56:14 +0200

    feat: add French
    Started by @camillecoyac in https://github.com/ipfs/js.ipfs.io/pull/106
    and finished at
    https://www.transifex.com/ipfs/js-ipfs-website/language/fr/

--- intl/config.js
+++ intl/config.js
@@ -4,6 +4,7 @@ module.exports = {
     { acronym: 'en', fullForm: 'English' },
     { acronym: 'pl', fullForm: 'Polski' },
     { acronym: 'it', fullForm: 'Italiano' },
+    { acronym: 'fr', fullForm: 'Français' },
     { acronym: 'zh-CN', fullForm: '简体中文' },
     { acronym: 'ko-KR', fullForm: '한국어' }
   ]
