2b7ab44a564a1e4248650b7f855e9e34e21b7a73
Pedro Santos <pedromiguelsousasantos@hotmail.com>
2018-09-28 18:01:30 +0100

    chore: add app codesandbox link (#178)
    This PR adds the first example of `What you can build with js-ipfs` section

--- src/shared/data/what-you-can-build/index.js
+++ src/shared/data/what-you-can-build/index.js
@@ -1,39 +1,7 @@
 const apps = [
   {
-    title: 'Example 1',
-    link: 'https://codesandbox.io/embed/qkj8z9l8kq'
-  },
-  {
-    title: 'Example 2',
-    link: 'https://codesandbox.io/embed/vv99onw18y'
-  },
-  {
-    title: 'Example 3',
-    link: 'https://codesandbox.io/embed/10jzy1v6x4'
-  },
-  {
-    title: 'Example 1',
-    link: 'https://codesandbox.io/embed/qkj8z9l8kq'
-  },
-  {
-    title: 'Example 2',
-    link: 'https://codesandbox.io/embed/vv99onw18y'
-  },
-  {
-    title: 'Example 3',
-    link: 'https://codesandbox.io/embed/10jzy1v6x4'
-  },
-  {
-    title: 'Example 1',
-    link: 'https://codesandbox.io/embed/qkj8z9l8kq'
-  },
-  {
-    title: 'Example 2',
-    link: 'https://codesandbox.io/embed/vv99onw18y'
-  },
-  {
-    title: 'Example 3',
-    link: 'https://codesandbox.io/embed/10jzy1v6x4'
+    title: 'Browser Video Streaming',
+    link: 'https://codesandbox.io/s/9296r558my'
   }
 ]
 
