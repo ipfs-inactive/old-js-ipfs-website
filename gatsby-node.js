c8a2e3a4032a2430987849b79048e97b0007df0b
Pedro Santos <pedro.santos@moxy.studio>
2018-11-15 18:52:51 +0000

    chore: testing another commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,7 +16,7 @@ module.exports.onCreateWebpackConfig = ({ actions, getConfig, stage }) => {
   let config = getConfig()
 
   // Name for non-entry chunk files. E.g. dynamic imports
-  config.output.chunkFilename = '[name].[chunkhash].js';
+  config.output.chunkFilename = '[name].[chunkhash].js'
 
   // Fix Gatsby setting `resolve.modules` to `path.resolve(__dirname, "node_modules")`
   // which causes module resolution errors when the npm tree is deduped
