d5bcfd298ab2de7fc8b00c8160a624cd87bdffbd
Vasco Santos <vasco.santos@moxy.studio>
2018-11-17 11:20:35 +0000

    chore: disable what you can build section

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,7 +4,7 @@ import ScreenSizeProvider from 'shared/components/screen-size-provider'
 import Hero from 'shared/components/hero-section'
 import Features from 'shared/components/features-section'
 import GettingStarted from 'shared/components/getting-started-section'
-import WhatYouCanBuild from 'shared/components/what-you-can-build-section'
+// import WhatYouCanBuild from 'shared/components/what-you-can-build-section'
 import WhatArePeopleBuilding from 'shared/components/what-are-people-building-section'
 import Gateway from 'shared/components/gateway-section'
 import PublicationsAndTalks from 'shared/components/publications-and-talks-section'
@@ -18,7 +18,7 @@ const Home = () => (
       <GettingStarted />
       <Gateway />
       <WhatArePeopleBuilding />
-      <WhatYouCanBuild />
+      { /* <WhatYouCanBuild /> */ }
       <PublicationsAndTalks />
       <Community />
     </ScreenSizeProvider>
