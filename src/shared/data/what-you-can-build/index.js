f788773b4d3e912436cae2da4cfe620687d6e091
Pedro Santos <pedro.santos@moxy.studio>
2018-11-20 16:17:00 +0000

    chore: update 'what you can build' examples to ipfs codepen account

--- src/shared/data/what-you-can-build/index.js
+++ src/shared/data/what-you-can-build/index.js
@@ -1,11 +1,11 @@
 const apps = [
   {
     title: 'Browser Video Streaming',
-    link: 'https://codepen.io/pedromssantos/embed/wYKydW'
+    link: 'https://codepen.io/ipfs/embed/XyVXRN'
   },
   {
     title: 'Exchange Files in Browser',
-    link: 'https://codepen.io/pedromssantos/embed/oaxqZK'
+    link: 'https://codepen.io/ipfs/embed/LXeGGL'
   }
 ]
 
