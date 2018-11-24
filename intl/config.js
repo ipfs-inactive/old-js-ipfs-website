2f8d8e2b6576ee3add3478d765f2bf47ad12bd24
Marcin Rataj <lidel@lidel.org>
2018-11-24 04:21:31 +0100

    feat: add Polish lang

--- intl/config.js
+++ intl/config.js
@@ -2,7 +2,7 @@ module.exports = {
   defaultLocale: 'en',
   availableLocales: [
     { acronym: 'en', fullForm: 'English' },
-    // { acronym: 'pl', fullForm: 'Polski' },
+    { acronym: 'pl', fullForm: 'Polski' },
     // { acronym: 'it', fullForm: 'Italiano' },
     { acronym: 'da', fullForm: 'Dansk' },
     { acronym: 'fr', fullForm: 'Français' },
