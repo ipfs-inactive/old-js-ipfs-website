c6abf990a5e08229f0c24b9239c82c91b653cf0d
Pedro Santos <pedro.santos@moxy.studio>
2019-04-30 15:13:27 +0100

    chore: update website hash (#258)

--- src/shared/constants/index.js
+++ src/shared/constants/index.js
@@ -1,6 +1,6 @@
 export const HASHES = {
   ipfsWebsite: 'QmYNQJoKGNHTpPxCBPh9KkDpaExgd2duMa3aF6ytMpHdao',
-  jsIpfsWebsite: 'QmcckYa6Z3myL9Hz9d1vuRAmJheAgam8YQCaAyusUwxQFt',
+  jsIpfsWebsite: 'QmTfgXTG2PN15mQEo7VCbSfc8RbJd9stabBcE4hzKBuiMX',
   awesomeIpfsWebsite: 'Qmcwy9pCEeyhj7rwSRtoAHVDskWpTumjbgLSBsS24PMY54',
   peerpadWebsite: 'QmXRTzu4dUFnA1nLR7ogifE1EeY2hEsVyQNkZu4CzF6g2v'
 }
