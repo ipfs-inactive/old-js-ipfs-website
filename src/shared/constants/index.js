7cfbb25933832404c6b1df740215523b2d656937
Pedro Santos <pedro.santos@moxy.studio>
2019-05-24 15:36:44 +0100

    chore: add new website hash

--- src/shared/constants/index.js
+++ src/shared/constants/index.js
@@ -1,6 +1,6 @@
 export const HASHES = {
   ipfsWebsite: 'QmYNQJoKGNHTpPxCBPh9KkDpaExgd2duMa3aF6ytMpHdao',
-  jsIpfsWebsite: 'QmTfgXTG2PN15mQEo7VCbSfc8RbJd9stabBcE4hzKBuiMX',
+  jsIpfsWebsite: 'Qmdg8cdfqyUUG9SWndNVV5CrpT8qPSzNNHmFSjnK6tfiX9',
   awesomeIpfsWebsite: 'Qmcwy9pCEeyhj7rwSRtoAHVDskWpTumjbgLSBsS24PMY54',
   peerpadWebsite: 'QmXRTzu4dUFnA1nLR7ogifE1EeY2hEsVyQNkZu4CzF6g2v'
 }
