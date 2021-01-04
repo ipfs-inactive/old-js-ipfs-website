378eeee2a21bb0340e66948aa6ee59f24775cec2
terichadbourne <terichad@gmail.com>
2021-01-04 11:33:11 -0500

    update protoschool links

--- src/shared/components/nav-bar/desktop/index.js
+++ src/shared/components/nav-bar/desktop/index.js
@@ -18,7 +18,7 @@ class DesktopNavBar extends Component {
       <div className={ navBarClasses } >
         <div className={ styles.navBarMenu }>
           <div className={ styles.link } onClick={ this.handleGettingStartedClick }> { messages.navBar.item1 } </div>
-          <Link href="https://proto.school/#/tutorials?course=ipfs">
+          <Link href="https://proto.school/course/ipfs">
             { messages.navBar.item2 }
           </Link>
           <Link href="https://github.com/ipfs/js-ipfs/tree/master/docs/core-api">
