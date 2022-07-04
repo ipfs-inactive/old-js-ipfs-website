15d384d47236d54459885ffa1a7bd0d10a12cc28
Marcin Rataj <lidel@lidel.org>
2022-07-04 16:00:30 +0200

    fix: link to community section in docs (#309)

--- src/shared/components/community-section/index.js
+++ src/shared/components/community-section/index.js
@@ -32,7 +32,7 @@ class Community extends Component {
             <div className={ styles.socialLinksContainer }>
               <p>{ messages.community.socialNetworksText }</p>
               <div className={ styles.socialLinks }>
-                <Button translationId="buttonIrcFreenode" href="http://webchat.freenode.net/?channels=%23ipfs" />
+                <Button translationId="buttonCommunity" href="https://docs.ipfs.io/community/" />
                 <Button translationId="buttonGithub" href="https://github.com/ipfs/js-ipfs" modifier="github" />
                 <Button translationId="buttonTwitter" href="https://twitter.com/IPFS" modifier="twitter" />
               </div>
