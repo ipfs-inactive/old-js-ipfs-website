2ce290fa3a6bfda3e5184081eb5b41dc44b6b270
Daniel N <2color@users.noreply.github.com>
2023-10-10 17:16:01 +0200

    Add deprecation warning and remove old website

--- src/shared/data/community/index.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/ipfs/contributors
-// Command used to generate: node scripts/bin/create js.ipfs.tech --org ipfs --duration 180 --spacing 2 --title "JS-IPFS Contributors" --width 114 --breakpoint 1
-
-const communityData = {
-  contributorsLink: 'https://ipfs.io/ipfs/QmVqKMP3SJ6TxvXkGvRLFKYVD3Yqve1kjA6SekrYENHdkz/projects/js.ipfs.io/'
-}
-
-export default communityData
--- src/shared/intl-config.js
+++
@@ -1,6 +0,0 @@
-// This is just an alias to '../../../intl/config.js'
-import intlConfig from '../../intl/config.js'
-
-const { defaultLocale, availableLocales } = intlConfig
-
-export { defaultLocale, availableLocales }
